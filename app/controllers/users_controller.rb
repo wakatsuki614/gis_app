3ba049e2e6e951fd7737a0fb5286d511e2f7dcfb
wakatsuki614 <wakatsuki.ryoya@gmail.com>
2022-02-20 16:58:28 +0900

    ユーザー登録・編集機能を作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = '新しいユーザーを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザーの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        render :edit
+      else
+        flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end
+    else
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
