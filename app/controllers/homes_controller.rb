fd33a1ab6ae85817301dd61ceb071d210465c37c
wakatsuki614 <91055744+wakatsuki614@users.noreply.github.com>
2022-03-04 12:00:50 +0900

    new information
    HOME画面へ新着情報を追加
    
    -新着情報の定義-
    limit(1).order(created_at: :desc)を使用し、作成日時が最も新しいスクール情報を１件表示する。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,6 @@ class HomesController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
 
   def index
+    @schools = School.all.limit(1).order(created_at: :desc)
   end
 end
