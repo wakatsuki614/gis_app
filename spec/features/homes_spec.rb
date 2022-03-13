7e48b365167074347cf38e6564fa5397600440c3
wakatsuki614 <wakatsuki.ryoya@gmail.com>
2022-03-13 21:03:08 +0900

    ドロップダウン動作テストを追加

--- spec/features/homes_spec.rb
+++ spec/features/homes_spec.rb
@@ -24,6 +24,60 @@ RSpec.feature "Homes", type: :feature do
     end
   end
 
+  feature "地域で検索" do
+    background do
+      visit root_path
+    end
+
+    scenario 'ドロップダウンが動作すること（北海道・東北地方）', js: true do
+      expect(page).not_to have_link "岩手県", href: "/search?keyword=岩手県"
+      click_button "北海道・東北地方"
+      expect(page).to have_link "岩手県", href: "/search?keyword=岩手県"
+    end
+
+    scenario 'ドロップダウンが動作すること（関東地方）', js: true do
+      expect(page).not_to have_link "栃木県", href: "/search?keyword=栃木県"
+      click_button "関東地方"
+      expect(page).to have_link "栃木県", href: "/search?keyword=栃木県"
+    end
+
+    scenario 'ドロップダウンが動作すること（甲信越・北陸地方）', js: true do
+      expect(page).not_to have_link "福井県", href: "/search?keyword=福井県"
+      click_button "甲信越・北陸地方"
+      expect(page).to have_link "福井県", href: "/search?keyword=福井県"
+    end
+
+    scenario 'ドロップダウンが動作すること（中部・東海地方）', js: true do
+      expect(page).not_to have_link "三重県", href: "/search?keyword=三重県"
+      click_button "中部・東海地方"
+      expect(page).to have_link "三重県", href: "/search?keyword=三重県"
+    end
+
+    scenario 'ドロップダウンが動作すること（近畿地方）', js: true do
+      expect(page).not_to have_link "奈良県", href: "/search?keyword=奈良県"
+      click_button "近畿地方"
+      expect(page).to have_link "奈良県", href: "/search?keyword=奈良県"
+    end
+
+    scenario 'ドロップダウンが動作すること（中国地方）', js: true do
+      expect(page).not_to have_link "山口県", href: "/search?keyword=山口県"
+      click_button "中国地方"
+      expect(page).to have_link "山口県", href: "/search?keyword=山口県"
+    end
+
+    scenario 'ドロップダウンが動作すること（四国地方）', js: true do
+      expect(page).not_to have_link "徳島県", href: "/search?keyword=徳島県"
+      click_button "四国地方"
+      expect(page).to have_link "徳島県", href: "/search?keyword=徳島県"
+    end
+
+    scenario 'ドロップダウンが動作すること（九州地方・沖縄）', js: true do
+      expect(page).not_to have_link "大分県", href: "/search?keyword=大分県"
+      click_button "九州地方・沖縄"
+      expect(page).to have_link "大分県", href: "/search?keyword=大分県"
+    end
+  end
+
   feature "新着情報" do
     given!(:school) { create(:school) }
 
