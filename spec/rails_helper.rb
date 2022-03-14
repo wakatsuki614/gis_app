be14e8c358d7582ab1f34fc051e22fb1ca9af3cc
wakatsuki614 <wakatsuki.ryoya@gmail.com>
2022-03-14 19:52:51 +0900

    新規作成 - System Spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,4 +76,12 @@ RSpec.configure do |config|
       Bullet.end_request
     end
   end
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
 end
