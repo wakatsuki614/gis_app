605fe4e46644ad546af1cad88030317b927a731b
wakatsuki614 <91055744+wakatsuki614@users.noreply.github.com>
2022-03-19 17:40:05 +0900

    dynamically change the title
    * 動的にタイトルを変えるため、application_helper.rbとViewファイルへ追記
    
    * ページタイトルが動的に表示されることのテストを追加
    
    * RuboCopの警告を修正

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe "#full_title" do
+    subject { full_title(page_title) }
+
+    context "page_titleが存在する場合に動的な表示がなされること" do
+      let(:page_title) { "sample" }
+
+      it { expect(subject).to eq "sample | GI Sports" }
+    end
+
+    context "page_titleが空白の場合に動的な表示がなされること" do
+      let(:page_title) { "" }
+
+      it { expect(subject).to eq "GI Sports" }
+    end
+
+    context "page_titleがnilの場合に動的な表示がなされること" do
+      let(:page_title) { nil }
+
+      it { expect(subject).to eq "GI Sports" }
+    end
+  end
+end
