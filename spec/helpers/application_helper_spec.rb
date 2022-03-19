2490bd107be8aefdd47d74353adaddff4001fcbd
wakatsuki614 <wakatsuki.ryoya@gmail.com>
2022-03-19 17:14:48 +0900

    RuboCopの警告を修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,16 +6,19 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     context "page_titleが存在する場合に動的な表示がなされること" do
       let(:page_title) { "sample" }
+
       it { expect(subject).to eq "sample | GI Sports" }
     end
 
     context "page_titleが空白の場合に動的な表示がなされること" do
       let(:page_title) { "" }
+
       it { expect(subject).to eq "GI Sports" }
     end
 
     context "page_titleがnilの場合に動的な表示がなされること" do
       let(:page_title) { nil }
+
       it { expect(subject).to eq "GI Sports" }
     end
   end
