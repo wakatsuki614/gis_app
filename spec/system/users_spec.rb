fc9b0bd7a51d174124cbef0edff1de2ba3c1ca34
wakatsuki614 <wakatsuki.ryoya@gmail.com>
2022-03-20 12:31:51 +0900

    関連テストの追加 - System

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  before do
-    visit signup_path
-  end
-
   describe "GET /signup" do
-    it '正しい情報を入力するとユーザー新規登録ができること' do
+    before do
+      visit signup_path
+    end
+
+    it '正しい情報を入力すると新規会員登録ができること' do
       fill_in "user[name]", with: "テストユーザー"
       fill_in "user[email]", with: "user@test.com"
       fill_in "user[password]", with: "password"
@@ -14,12 +14,25 @@ RSpec.describe "Users", type: :system do
       expect { click_button "登録する" }.to change { User.count }.by(1)
     end
 
-    it '誤った情報ではユーザー新規登録ができないこと' do
+    it '誤った情報では新規会員登録ができないこと' do
       fill_in "user[name]", with: ""
       fill_in "user[email]", with: ""
       fill_in "user[password]", with: ""
       fill_in "user[password_confirmation]", with: ""
       expect { click_button "登録する" }.to change { User.count }.by(0)
+    end
+  end
+
+  describe "GET /users/:id/edit" do
+    let(:user) { create(:user) }
+
+    it '会員退会処理されること' do
+      visit login_path
+      fill_in "session[email]", with: user.email
+      fill_in "session[password]", with: user.password
+      find(".login-test").click
+      visit edit_user_path(user)
+      expect { click_on "退会する" }.to change { User.count }.by(-1)
     end
   end
 end
